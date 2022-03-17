c93fa5d9b4fbe274bce6be29abc2d9de34b72aec
noahfranz13 <noahfranz13@gmail.com>
2022-03-17 16:38:57 -0400

    check ids

--- pipeline/find_cadence.py
+++ pipeline/find_cadence.py
@@ -17,7 +17,7 @@ def findCadences(targets, band='L'):
     cadences = {}
     for tt in targetObjs:
         print()
-        print("CADENCE FOR : ", tt)
+        print("CADENCE FOR : ", tt.name)
         print()
 
         observations = c.fetch_observations_by_target(tt.id)
@@ -45,8 +45,8 @@ def findCadences(targets, band='L'):
                 if len(metas) > 0:
                     meta = metas[0] # get filename of first observation in the cadence
                     targetname = meta.filename().split('/')[-1].split('_')[-2]
-                    print(targetname, tt)
-                    if targetname == tt:
+                    print(meta.id, tt.id)
+                    if targetname == tt.id:
                         goodCad = cad
                         break
 
