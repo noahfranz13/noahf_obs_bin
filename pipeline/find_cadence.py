8c0000bc223c737db1b4cc7e614f5c5c0d1006f1
noahfranz13 <noahfranz13@gmail.com>
2022-03-22 16:38:22 -0400

    add check that all files are only datag paths

--- pipeline/find_cadence.py
+++ pipeline/find_cadence.py
@@ -69,18 +69,12 @@ def findCadences(targets, band):
         else:
             for metas in goodCad.metas:
                 for meta in metas:
-                    file = meta.filename()
-                    print("Found: ", file)
-                    cadenceList.append(file)
+                    # check the file paths in the cadence dictionary so that each cadence only has datag files
+                    if file[0:6] == '/datag':
+                        file = meta.filename()
+                        print("Found: ", file)
+                        cadenceList.append(file)
 
-        '''# check the file paths in the cadence dictionary so that each cadence only has datag files
-        if len(cadenceList) > 6:
-            betterCadenceList = []
-            for path in cadenceList:
-                if path[0:6] == '/datag':
-                    betterCadenceList.append(path)
-
-            cadenceList = betterCadenceList'''
 
         if len(cadenceList) > 0:
             if not os.path.exists(outDirPath):
